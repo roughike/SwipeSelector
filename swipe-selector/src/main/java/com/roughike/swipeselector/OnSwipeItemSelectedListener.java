8956166a505c247ec82cde5e7391e9614ebc63dd
Iiro <iiro.krankka@gmail.com>
2016-02-26 19:46:42 +0200

    Added some commenting, because why not.

--- swipe-selector/src/main/java/com/roughike/swipeselector/OnSwipeItemSelectedListener.java
+++ swipe-selector/src/main/java/com/roughike/swipeselector/OnSwipeItemSelectedListener.java
@@ -19,6 +19,10 @@ package com.roughike.swipeselector;
 public interface OnSwipeItemSelectedListener {
     /**
      * The method being called when currently visible {@link SwipeItem} changes.
+     * This listener won't be fired until the user changes the selected item the
+     * first time. So you won't get this event when you're just initialized the
+     * SwipeSelector.
+     *
      * @param item the new visible {@link SwipeItem}
      */
     void onItemSelected(SwipeItem item);
